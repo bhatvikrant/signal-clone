ecf55b2b1cb834e784996510be8fe2b817094532
Vikrant Bhat <vik.bhat223@gmail.com>
2021-06-05 04:33:12 +0530

    AddChat screen done

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./screens/Login";
 import Register from "./screens/Register";
 import Home from "./screens/Home";
+import AddChat from "./screens/AddChat";
 
 const Stack = createStackNavigator();
 
@@ -30,6 +31,7 @@ export default function App() {
 				<Stack.Screen name="Login" component={Login} />
 				<Stack.Screen name="Register" component={Register} />
 				<Stack.Screen name="Home" component={Home} />
+				<Stack.Screen name="AddChat" component={AddChat} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,8 +1,17 @@
 import React, { useLayoutEffect } from "react";
 import { TouchableOpacity } from "react-native";
 import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+
+// ICONS
+import { AntDesign, SimpleLineIcons } from "@expo/vector-icons";
+
+// RN ELEMENTS
 import { Avatar } from "react-native-elements";
+
+// COMPONENTS
 import CustomListItem from "../components/CustomListItem";
+
+// FIREBASE
 import { auth } from "../firebase";
 
 const Home = ({ navigation }) => {
@@ -31,8 +40,30 @@ const Home = ({ navigation }) => {
 					</TouchableOpacity>
 				</View>
 			),
+			headerRight: () => (
+				<View
+					style={{
+						flexDirection: "row",
+						justifyContent: "space-between",
+						width: 80,
+						marginRight: 20,
+					}}
+				>
+					<TouchableOpacity activeOpacity={0.5}>
+						<AntDesign name="camerao" size={24} color="black" />
+					</TouchableOpacity>
+					<TouchableOpacity activeOpacity={0.5}>
+						<SimpleLineIcons
+							name="pencil"
+							size={24}
+							color="black"
+							onPress={() => navigation.navigate("AddChat")}
+						/>
+					</TouchableOpacity>
+				</View>
+			),
 		});
-	}, []);
+	}, [navigation]);
 
 	return (
 		<SafeAreaView>
